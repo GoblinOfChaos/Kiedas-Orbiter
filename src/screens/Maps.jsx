2813dc9d4b8776630fa8bed4dbdf7f6b17a3952a
glowseeker <ecrn@tuta.io>
2026-07-25 20:28:53 +0200

    Maps: remove folder icon from header bar

--- src/screens/Maps.jsx
+++ src/screens/Maps.jsx
@@ -552,11 +552,6 @@ export default function Maps() {
                   title="Configurations">
                   <Layers size={16} />
                 </button>
-                <button onClick={() => invoke('open_map_configs_folder')}
-                  className="p-2 rounded-lg bg-kronos-bg/80 backdrop-blur text-kronos-dim hover:text-kronos-text border border-white/5 transition-colors"
-                  title="Open map configs folder">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>
-                </button>
               </div>
 
               {mode === 'addMarker' && pendingConfigId && (
